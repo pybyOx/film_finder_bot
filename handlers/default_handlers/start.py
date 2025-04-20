534662ce8f456082913ca5e560a80ce7156cfb80
Roman1 <us.rom.tim@yandex.ru>
2025-04-20 15:26:33 +0700

    fixed start file

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -2,8 +2,13 @@ from telebot.types import Message
 from loader import bot
 
 
-@bot.message_handler(func=lambda message: message.text.lower() in ['hello-world', 'start', 'привет',
+@bot.message_handler(func=lambda message: message.text.lower() in ['start', 'привет',
                                                                    'добрый день', 'здравствуй'])
 def bot_start(message: Message):
     bot.reply_to(message, f"🎬 Привет, {message.from_user.full_name}! \nЯ FilmBuddy — твой гид по миру кино."
                           f"\nДля просмотра доступных команд напиши /help")
+
+
+@bot.message_handler(commands=["helloworld"])
+def bot_echo(message: Message):
+    bot.reply_to(message, "Hello, world!")
